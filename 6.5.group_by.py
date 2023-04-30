67be2474a8fffb8a30c10c490a475daf3902d046
Shai Axelrod <Saxelrod@g.jct.ac.il>
2023-04-30 12:14:28 +0300

    fix group_by
    1.replace i with item
    2.don't use list comprehension for side effect
    3.add run example

--- 6.5.group_by.py
+++ 6.5.group_by.py
@@ -8,6 +8,11 @@ def group_by(func, iterable):
     :return: A dictionary where the keys are the results of applying `func` to the elements of `iterable`,
             and the values are lists of the elements that have that key.
     """
-    grouped_dict = {func(i): [] for i in iterable}
-    [grouped_dict[func(i)].append(i) for i in iterable]
+    grouped_dict = {func(item): [] for item in iterable}
+    for item in iterable:
+        grouped_dict[func(item)].append(item)
     return grouped_dict
+
+
+if __name__ == "__main__":
+    print(group_by(len, ["hi", "bye", "yo", "try"]))
