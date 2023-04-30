f91492bd60c16785040766958760be0440f8fbfd
Shai Axelrod <Saxelrod@g.jct.ac.il>
2023-04-30 12:02:38 +0300

    fix long cat - add blank line

--- 6.3.long_cat_is_long.py
+++ 6.3.long_cat_is_long.py
@@ -4,12 +4,14 @@ WORD_PATTERN = "[a-zA-Z]+"
 
 
 def count_words(text):
+
     """
     Returns the words length in a given text
     :param text: The text to check the word lengths in it.
     :return: A dictionary where the keys are the words in the text (converted to lowercase) and the values
         are the length of the words.
     """
+
     # first - clean the text from non-letters chars.
     clean_txt = [re.match(WORD_PATTERN, word).group(0)
                  for line in text.split("\n") for word in line.split(" ")
